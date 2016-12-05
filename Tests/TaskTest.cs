be22c2b4b57597016b85d640858e7205fd402884
Anthony Bottemiller and Jesse McKay <anthony+jesse@>
2016-12-05 11:34:04 -0800

    initial commit

---
+++ Tests/TaskTest.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ToDoList
+{
+  public class ToDoTest : IDisposable
+  {
+    public ToDoTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todolist_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+        public void Test_DatabaseEmptyAtFirst()
+        {
+          //Arrange, Act
+          int result = Task.GetAll().Count;
+
+          //Assert
+          Assert.Equal(0, result);
+        }
+        public void Dispose()
+          {
+            Task.DeleteAll();
+          }
+
+  }
+}
